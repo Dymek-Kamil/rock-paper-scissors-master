f7855498fa279225c89586574865aa65fe8c71a3
Dymek-Kamil <kamil.dymek33@gmail.com>
2023-11-15 15:31:27 +0100

    HTML/CSS/JS

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const btnRules = document.querySelector(".btn--rules");
 const btnModalClose = document.querySelector(".btn--close");
 const modalRules = document.querySelector(".rules");
 const overlay = document.querySelector(".overlay");
+const optContainer = document.querySelector(".options--container");
+const finalContainer = document.querySelector(".final--container");
+const playerSection = document.querySelector(".player--section");
 
 // Opening Modal window
 btnRules.addEventListener("click", function () {
@@ -20,3 +23,24 @@ function closeModal() {
 
 btnModalClose.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+// Clicking element
+optContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".option");
+  console.log(clicked);
+
+  if (!clicked) return;
+
+  const choice = clicked.dataset.choice;
+  console.log(choice);
+
+  optContainer.classList.add("hidden");
+  finalContainer.classList.remove("hidden");
+
+  playerSection.insertAdjacentHTML(
+    "beforeend",
+    `<div class="option ${choice} final--option">
+  <img src="images/icon-${choice}.svg" alt="${choice}"/>
+</div>`
+  );
+});
