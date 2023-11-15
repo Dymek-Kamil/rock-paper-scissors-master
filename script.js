d87782fd5fca0607b219826b7eaad63219b5a7b4
Dymek-Kamil <kamil.dymek33@gmail.com>
2023-11-15 14:47:07 +0100

    Modal window

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+// Selecting HTML objects
+const btnRules = document.querySelector(".btn--rules");
+const btnModalClose = document.querySelector(".btn--close");
+const modalRules = document.querySelector(".rules");
+const overlay = document.querySelector(".overlay");
+
+// Opening Modal window
+btnRules.addEventListener("click", function () {
+  overlay.classList.remove("hidden");
+  modalRules.classList.remove("hidden");
+});
+
+// Closing Modal window
+function closeModal() {
+  overlay.classList.add("hidden");
+  modalRules.classList.add("hidden");
+}
+
+btnModalClose.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
